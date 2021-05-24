46d51a796c1d604d4d3e24596ecbbcceb6be0b7b
404-09 <404-09@DESKTOP-FUBJIA2>
2021-05-24 10:59:44 +0900

    Java Study day4

---
+++ day4/src/ch05/ContinueBasic.java
@@ -0,0 +1,43 @@
+package ch05;
+
+public class ContinueBasic {
+
+	public static void main(String[] args) {
+		int count = 0;
+		
+		for (int num = 1; num<100; num++) {
+			if (((num % 5) != 0) || ((num % 7) != 0))
+				continue;
+			count++;
+			System.out.println(num);
+		}
+		System.out.println("count: " + count);
+		
+		
+	
+//		int sum = 0;    
+//		for (int num1 = 1; num1<65; num1++) {
+//			if (num1 % 2 == 1) {
+//				sum += num1;
+//				if (sum < 1000) {
+//					System.out.println(sum);
+//				}
+//				else System.out.println(sum);
+//				
+//			}				
+//		}
+		
+		
+		
+		int sum = 0;    
+		for (int num1 = 1; sum<1000; num1++) {
+			if (num1 % 2 == 1) {
+				sum += num1;
+				System.out.println(sum);
+			}				
+		}
+		
+	}
+}
+
+
